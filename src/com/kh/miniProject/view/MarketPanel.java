af971ea06afc39f804308cd993f23837c38dbfba
user2 <user2@KH_C>
2019-02-08 21:31:48 +0900

    이미지 추가 및 상점수정

--- src/com/kh/miniProject/view/MarketPanel.java
+++ src/com/kh/miniProject/view/MarketPanel.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import com.kh.miniProject.model.vo.member.Member;
 
@@ -21,15 +23,49 @@ public class MarketPanel extends JPanel {
 	JButton mo = new JButton("¶±ººÀÌ±â±¸");
 	private int tkLv = 1;
 	JButton mo1 = new JButton("Æ¢±è±â±¸");
+	//20190208 Ãß°¡
+	private int odLv=1;
+	JButton mo2=new JButton("¿Àµ­±â±¸");
+	private int rmLv=1;
+	JButton mo3=new JButton("¶ó¸é±â±¸");
+	
+	private int tbkup=1;
+	JButton mo4=new JButton("¶±ººÀÌÆÇ ¾÷±Û");
+	private int tkup=1;
+	JButton mo5=new JButton("Æ¢±èÆÇ ¾÷±Û");
+	private int odup=1;
+	JButton mo6=new JButton("¿Àµ­ÆÇ ¾÷±Û");
+	private int rmup=1;
+	JButton mo7=new JButton("¶ó¸éÆÇ ¾÷±Û");
+	
 	JPanel mPanel;
 	private int[] equipsLv;
 	private EquipSetting e;
-
+	
+	
+	//¶±ººÀÌ ±â±¸
 	private Image[] equipsImages = { new ImageIcon("images/1Lvtbk.jpg").getImage().getScaledInstance(200, 200, 0),
 			new ImageIcon("images/2Lvtbk.jpg").getImage().getScaledInstance(200, 200, 0) };
-
+	//Æ¢±è ±â±¸
 	private Image[] equipsImages1 = { new ImageIcon("images/1Lvfried.jpg").getImage().getScaledInstance(200, 200, 0),
 			new ImageIcon("images/2Lvfried.jpg").getImage().getScaledInstance(200, 200, 0) };
+	//¿Àµ­ ±â±¸
+	private Image[] equipsImages2 = { new ImageIcon("images/¿Àµ­Lv1.jpg").getImage().getScaledInstance(200, 200, 0),
+			new ImageIcon("images/¿Àµ­Lv2.jpg").getImage().getScaledInstance(200, 200, 0) };
+	//¶ó¸é ±â±¸
+	private Image[] equipsImages3 = { new ImageIcon("images/¶ó¸éLv1.jpg").getImage().getScaledInstance(200, 200, 0),
+			new ImageIcon("images/¶ó¸éLv2.jpg").getImage().getScaledInstance(200, 200, 0) };
+	
+	//¶±ººÀÌ ÆÇ
+	private Image[] equipsImages4= {new ImageIcon("images/tbk1.png").getImage().getScaledInstance(200,200,0),
+			new ImageIcon("images/tbk2.png").getImage().getScaledInstance(200,200,0) };
+	//Æ¢±è ÆÇ
+	private Image[] equipsImages5= {new ImageIcon("images/fried1.png").getImage().getScaledInstance(200,200,0),
+			new ImageIcon("images/fried2.png").getImage().getScaledInstance(200,200,0)};
+	
+	//¿Àµ­ ÆÇ
+	
+	//¶ó¸é ÆÇ
 
 	public MarketPanel(MainFrame mf, Member m) {
 		e = new EquipSetting();
@@ -67,7 +103,7 @@ public class MarketPanel extends JPanel {
 		} else if (equipsLv[0] == 2) {
 			mo.setIcon(new ImageIcon(equipsImages[1]));
 		}
-		mo.setBounds(200, 200, 200, 200);
+		mo.setBounds(20, 60, 180, 180);
 		mo.removeActionListener(e);
 		mo.addActionListener(e);
 
@@ -77,12 +113,80 @@ public class MarketPanel extends JPanel {
 		} else if (equipsLv[1] == 2) {
 			mo1.setIcon(new ImageIcon(equipsImages1[1]));
 		}
-		mo1.setBounds(500, 200, 200, 200);
+		mo1.setBounds(210, 60, 180, 180);
 		mo1.removeActionListener(e);
 		mo1.addActionListener(e);
+		
+		//¿Àµ­
+		if (equipsLv[2] == 1) {
+			mo2.setIcon(new ImageIcon(equipsImages2[0]));
+		} else if (equipsLv[2] == 2) {
+			mo2.setIcon(new ImageIcon(equipsImages2[1]));
+		}
+		mo2.setBounds(400, 60, 180, 180);
+		mo2.removeActionListener(e);
+		mo2.addActionListener(e);
+		
+		//¶ó¸é		
+		if (equipsLv[3] == 1) {
+			mo3.setIcon(new ImageIcon(equipsImages3[0]));
+		} else if (equipsLv[3] == 2) {
+			mo3.setIcon(new ImageIcon(equipsImages3[1]));
+		}
+		mo3.setBounds(590, 60, 180, 180);
+		mo3.removeActionListener(e);
+		mo3.addActionListener(e);
+		
+		//¶±ººÀÌÆÇ
+		if(equipsLv[4]==1) {
+			mo4.setIcon(new ImageIcon(equipsImages4[0]));
+		}else if(equipsLv[4]==2) {
+			mo4.setIcon(new ImageIcon(equipsImages4[1]));
+		}
+		mo4.setBounds(20,360, 180,180);
+		mo4.removeActionListener(e);
+		mo4.addActionListener(e);
+		
+		//Æ¢±è ÆÇ		
+		/*if(equipsLv[5]==1) {
+			mo5.setIcon(new ImageIcon(equipsImages5[0]));
+		}else if(equipsLv[5]==2) {
+			mo5.setIcon(new ImageIcon(equipsImages5[1]));
+		}
+		mo5.setBounds(220,360, 180,180);
+		mo5.removeActionListener(e);
+		mo5.addActionListener(e);*/
+		
 
 		panel.add(mo);
 		panel.add(mo1);
+		panel.add(mo2);
+		panel.add(mo3);
+		panel.add(mo4);
+		panel.add(mo5);
+		panel.add(mo6);
+		panel.add(mo7);
+		
+		//¶óº§
+		JLabel label=new JLabel();
+		label.setText("¶±ººÀÌ ¾÷±×·¹ÀÌµå");
+		label.setBounds(50,160,200,200);
+		panel.add(label);
+		JLabel label1=new JLabel();
+		label1.setText("Æ¢±è ¾÷±×·¹ÀÌµå");
+		label1.setBounds(250,160,200,200);
+		panel.add(label1);
+		JLabel label2=new JLabel();
+		label2.setText("¿Àµ­ ¾÷±×·¹ÀÌµå");
+		label2.setBounds(450,160,200,200);
+		panel.add(label2);
+		JLabel label3=new JLabel();
+		label3.setText("¶ó¸é ¾÷±×·¹ÀÌµå");
+		label3.setBounds(630,160,200,200);
+		panel.add(label3);
+//		panel.setVisible(true);
+		
+		
 	}
 
 	class EquipSetting implements ActionListener {
@@ -103,6 +207,42 @@ public class MarketPanel extends JPanel {
 				if (equipsLv[1] == 1) {
 					equipsLv[1] += 1;
 				} else {
+					System.out.println("lv max error");
+				}
+				refresh();
+			}
+			if(e.getActionCommand().equals("¿Àµ­±â±¸")) {
+				System.out.println("od");
+				if(equipsLv[2]==1) {
+					equipsLv[2]+=1;
+				}else {
+					System.out.println("lv max error");
+				}
+				refresh();
+			}
+			if(e.getActionCommand().equals("¶ó¸é±â±¸")) {
+				System.out.println("rm");
+				if(equipsLv[3]==1) {
+					equipsLv[3]+=1;
+				}else {
+					System.out.println("lv max error");
+				}
+				refresh();				
+			}
+			if(e.getActionCommand().equals("¶±ººÀÌÆÇ ¾÷±Û")) {
+				System.out.println("tbkup");
+				if(equipsLv[4]==1) {
+					equipsLv[4]+=1;
+				}else {
+					System.out.println("lv max error");
+				}
+				refresh();
+			}
+			if(e.getActionCommand().equals("Æ¢±èÆÇ ¾÷±Û")) {
+				System.out.println("tkup");
+				if(equipsLv[5]==1) {
+					equipsLv[5]+=1;
+				}else {
 					System.out.println("lv max error");
 				}
 				refresh();
