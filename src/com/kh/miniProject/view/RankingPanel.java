d2136ffbad47daabc97faa164e9e3c3a148a90cc
sy <sy@DESKTOP-4G6SI59>
2019-02-15 20:06:36 +0900

    gg

--- src/com/kh/miniProject/view/RankingPanel.java
+++ src/com/kh/miniProject/view/RankingPanel.java
@@ -1,9 +1,6 @@
 package com.kh.miniProject.view;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.EOFException;
@@ -19,10 +16,14 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.Timer;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 
 import com.kh.miniProject.model.vo.member.Member;
+import com.kh.miniProject.music.Music;
 
 
 
@@ -40,35 +41,110 @@ public class RankingPanel extends JPanel {
 	private JPanel rPanel;
 	private JLabel a;
 	private Font f1;
-	private Image backImage = new ImageIcon("C:\\Users\\y_s_r\\OneDrive\\바탕 화면\\back.jpg"/*"images/ranking.jpg"*/).getImage();
+	private Image backImage = new ImageIcon("images/rank.jpg"/*"images/ranking.jpg"*/).getImage();
 	private JScrollPane table;
 	private MainFrame mf;
+	
+
+	
+	private Timer rangTimer;
+	private int rangIndex;
+	private JLabel[] medalLabel;
+	private Image[] medalImage = {
+								new ImageIcon("images/best.png").getImage().getScaledInstance(30, 30, 0),
+								new ImageIcon("images/second.png").getImage().getScaledInstance(30, 30, 0),
+								new ImageIcon("images/third.png").getImage().getScaledInstance(30, 30, 0)
+	};
+	
+	
+	
+	private DefaultTableCellRenderer celAlignCenter;
+	private DefaultTableCellRenderer[] rankingRenderer;
 
 	public RankingPanel(MainFrame mf) {
 		this.mf = mf;
+		this.rPanel = this;
+		medalLabel = new JLabel[3];
+		/*medalLabel = new JLabel[3];*/
+		this.setLayout(null);
+		
 		memberRead();
 		sortScore();
 
-
+	    celAlignCenter = new DefaultTableCellRenderer();   
+	    celAlignCenter.setHorizontalAlignment(JLabel.CENTER);//가운데정렬
+	    //celAlignCenter.setIcon(new ImageIcon(medalImage[0]));
+	    
+	    rankingRenderer = new DefaultTableCellRenderer[3];
+	    
+	    for(int i=0;i<rankingRenderer.length;i++) {
+	    	rankingRenderer[i] = new DefaultTableCellRenderer();
+	    	rankingRenderer[i].setIcon(new ImageIcon(medalImage[i]));
+	    }
+	    
+	    
 		tb = new JTable();
-		this.add(tb);
-
-		tb.setBounds(280,200,450,300);
-
+		tb.setBounds(175,200,700,230);
 		defaultTable = new DefaultTableModel(menu, tempMember.size());
-
 		tb.setModel(defaultTable);
 
+		int max =0;
+		
 		for(int i=0; i<tempMember.size();i++) {
+			
+			
+			if(i>=3) {
 			defaultTable.setValueAt(i+1, i, 0);
 			defaultTable.setValueAt(tempMember.get(i).getMemberId(), i, 1);
 			defaultTable.setValueAt(tempMember.get(i).getMaxStage(), i, 2);
 			defaultTable.setValueAt(tempMember.get(i).getGold(), i, 3);
+		
+			}
+			
+			/*if(tempMember.get(i).getMemberId().length()>max) {
+				max = tempMember.get(i).getMemberId().length();
+				System.out.println("max : " + max);
+			}*/
+			tb.setRowHeight(i, 40);		
 		}
+		for(int i=0;i<4;i++) {
+			tb.getColumnModel().getColumn(i).setCellRenderer(celAlignCenter);		
+		}
+		
+		tb.getColumn("아이디").setPreferredWidth(max);
+		tb.getColumn("최고스테이지").setPreferredWidth(6);
+		tb.getColumn("등수").setPreferredWidth(1);
+		
+		
+		rangTimer = new Timer(500,new RankingTimer());
+		rangTimer.start();
+		rangIndex = 2;
+		
+		int temp =50;
+		
+		for(int i=0;i<medalLabel.length;i++) {
+			medalLabel[i] = new JLabel("");
+			medalLabel[i].setSize(30, 30);
 
+			temp-=50;
+			this.add(medalLabel[i]);
+		}
+		
+		table = new JScrollPane(tb,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		table.setBounds(175,200,650,230);
+		table.setOpaque(false);
+		tb.setOpaque(false);
+		
+		
+
+		this.add(table);
+		
+		
+		table.setOpaque(false);
+		table.getViewport().setOpaque(false);
+		
 		this.setSize(1024,768);
-		this.setLayout(null);
-
+		
 
 		JTableHeader table1 = tb.getTableHeader();
 		table1.setBackground(Color.black); 
@@ -81,17 +157,19 @@ public class RankingPanel extends JPanel {
 		this.add(btn1);
 		btn1.addActionListener(new BackBtn());
 
-		f1 = new Font("궁서체",Font.BOLD,50);
+		f1 = new Font("Dialog",Font.BOLD,50);
 
-		a = new JLabel("부글부글 분식");
+		a = new JLabel("TOTAL RANKING");
 		a.setForeground(Color.RED); //얘가 윗것보다 아래에 있어야 함  
-
 		a.setSize(500,100);
-		a.setLocation(320,50);
-
+		a.setLocation(300,50);
 		a.setFont(f1);
+		
 		rPanel = this;
 		this.add(a);
+		
+		Music music = new Music("드럼.mp3",false);
+		music.start();
 	}		
 
 	@Override
@@ -99,7 +177,12 @@ public class RankingPanel extends JPanel {
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		g.drawImage(backImage,0,0, null);
+		
+		
 	}
+	
+	
+
 
 	public void sortScore() {
 		tempMember.sort(new Comparator<Member>(){
@@ -150,12 +233,41 @@ public class RankingPanel extends JPanel {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getActionCommand().equals("Main")) {
-				new ChangePanel().changePanel(mf, rPanel, new LoginPanel(mf));
+				ChangePanel.changePanel(mf, rPanel, new LoginPanel(mf));
 			}
 		}
+	}
+	
+	class RankingTimer implements ActionListener{
+		private int time = 3;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			/*DefaultTableCellRenderer celAlignCenter = new DefaultTableCellRenderer();   
+			celAlignCenter.setHorizontalAlignment(JLabel.CENTER);//가운데정렬
+*/			
+			if(time==0) {
+				rangTimer.stop();
+			}else {
+				defaultTable.setValueAt(rangIndex+1, rangIndex, 0);
+				defaultTable.setValueAt(tempMember.get(rangIndex).getMemberId(), rangIndex, 1);
+				defaultTable.setValueAt(tempMember.get(rangIndex).getMaxStage(), rangIndex, 2);
+				defaultTable.setValueAt(tempMember.get(rangIndex).getGold(), rangIndex, 3);
+				tb.getColumnModel().getColumn(0).setCellRenderer(rankingRenderer[rangIndex]);
+			
+				//tb.getCellRenderer(0, 0)
+				
+				//getCellRenderer ;
+				
+			rangIndex--;
+			time--;
+			}
+		}
+	
 
 	}
-}
+}	
+
 
 
 
