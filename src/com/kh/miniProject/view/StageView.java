272044660d0344e6847695ff09e3475fce293592
sy <sy@DESKTOP-4G6SI59>
2019-02-16 20:58:39 +0900

    ㅎㅎㅎㅎ

--- src/com/kh/miniProject/view/StageView.java
+++ src/com/kh/miniProject/view/StageView.java
@@ -3,6 +3,8 @@ package com.kh.miniProject.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -37,6 +39,8 @@ public class StageView extends JPanel{
 			new ImageIcon("images/nine.png").getImage().getScaledInstance(165, 150, 0),
 			new ImageIcon("images/zero.png").getImage().getScaledInstance(165, 150, 0)};
 	private Image backImage = new ImageIcon("images/LoguinFrame01_01.jpg").getImage();
+	
+	private JButton testButton;
 
 	public StageView(MainFrame mf,Member m) {
 		this.m = m;
@@ -97,6 +101,21 @@ public class StageView extends JPanel{
 
 		this.sView = this;
 
+		testButton=new JButton("테스트)엔딩");
+		
+		testButton.setBounds(700, 500, 200, 200);
+		testButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				ChangePanel.changePanel(mf, sView, new EndingPanel(mf));
+				music.close();
+			}
+			
+		});
+		
+		this.add(testButton);
 
 		//스테이지1 번 버튼 선택 시 GameView로 changePanel 실행
 		for (int i = 0; i < stageBtn.length; i++) {
